67d4fb9e251b554d4c639395eed5eafa4150e256
CrsOystr <nicmetz@jackmoves.com>
2015-09-17 13:45:32 -0600

    Got camera set up and witch rotation but no controls

--- WitchAndKnight/Assets/KnightController.cs
+++ WitchAndKnight/Assets/KnightController.cs
@@ -5,11 +5,9 @@ public class KnightController : MonoBehaviour {
 
 	public float moveSpeed;
 
-	private Transform trans;
 
 	// Use this for initialization
 	void Start () {
-		trans = GetComponent<Transform> ();
 	}
 	
 	// Update is called once per frame
@@ -18,13 +16,21 @@ public class KnightController : MonoBehaviour {
 		float vertInputRaw = Input.GetAxisRaw ("Vertical"); // snapped to -1,0,1
 		float horizInputRaw = Input.GetAxisRaw ("Horizontal"); // snapped to -1,0,1
 
+		Vector3 moveVector = new Vector3 ();
 
 		if (vertInputRaw != 0) {
-			trans.Translate(Vector3.forward * (vertInputRaw * moveSpeed), 0);
+			//transform.Translate(Vector3.forward * (vertInputRaw * moveSpeed), 0);
+			moveVector += Vector3.forward * (vertInputRaw * moveSpeed);
+
+
 		} 
 		if (horizInputRaw != 0) {
-				trans.Translate(Vector3.right * (horizInputRaw * moveSpeed), 0);
+			//transform.Translate(Vector3.right * (horizInputRaw * moveSpeed), 0);
+			moveVector += Vector3.right * (horizInputRaw * moveSpeed);
 		}
+
+		transform.GetComponent<CharacterController>().Move(moveVector);
+
 
 	}
 }
---
+++ WitchAndKnight/Assets/WitchController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WitchController : MonoBehaviour {
+
+	public float moveSpeed;
+
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool vertInputRaw = Input.GetKeyDown (KeyCode.E); // snapped to -1,0,1
+		bool horizInputRaw = Input.GetKeyDown (KeyCode.Q);
+
+		float speedz = 20; 
+		if (vertInputRaw == true) {
+			//transform.Translate(Vector3.forward * (vertInputRaw * moveSpeed), 0);
+			//moveVector += Vector3.forward * (vertInputRaw * moveSpeed);
+			speedz = 50;
+			
+			
+		} 
+		if (horizInputRaw == true) {
+			//transform.Translate(Vector3.right * (horizInputRaw * moveSpeed), 0);
+			//moveVector += Vector3.right * (horizInputRaw * moveSpeed);
+			speedz = -50;
+
+		}
+
+
+		//transform.parent.GetComponentInParent<Transform> ().Rotate (Vector3.up * speedz * moveSpeed*Time.deltaTime);
+		transform.parent.GetComponentInParent<Transform> ().Rotate (Vector3.up *Time.deltaTime * 20);
+
+	}
+}
