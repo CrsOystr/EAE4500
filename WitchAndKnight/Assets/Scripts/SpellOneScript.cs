278c6b28105280f8285f96c817d6aa615bf20339
CrsOystr <nicmetz@jackmoves.com>
2015-09-20 15:51:20 -0600

    Added a spell

---
+++ WitchAndKnight/Assets/Scripts/SpellOneScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpellOneScript : MonoBehaviour {
+
+	float timeToLive;
+
+	// Use this for initialization
+	void Start () {
+		Object.Destroy(gameObject, 0.4f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.forward * 10 *Time.deltaTime);
+	}
+}
---
+++ WitchAndKnight/Assets/Scripts/WitchController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WitchController : MonoBehaviour {
+
+	public float moveSpeed;
+	public float spellSpeed;
+	public float spellCooldown;
+	public GameObject spellOne;
+
+    
+	// Use this for initialization
+	void Start () { 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float vertInputRaw = Input.GetAxis ("WitchLeft");
+		float horizInputRaw = Input.GetAxis ("WitchRight");
+		float spellOneInput = Input.GetAxis ("Spell 1");
+
+		float moveVector = 0; 
+
+		if (vertInputRaw != 0) {
+
+			moveVector  = 50;
+		} 
+		if (horizInputRaw != 0) {
+
+			moveVector = -50;
+		}
+		if (spellOneInput != 0) {
+			GameObject newProjectile = Instantiate( spellOne, transform.position , transform.rotation ) as GameObject;
+		}
+
+		transform.parent.GetComponentInParent<Transform> ().Rotate (Vector3.up * Time.deltaTime * moveVector *  moveSpeed);
+
+	}
+}
