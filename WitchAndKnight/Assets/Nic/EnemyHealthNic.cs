41e07278b8a8f4df2d763751a561f6e5a0e2e14b
CrsOystr <nicmetz@jackmoves.com>
2015-09-28 16:13:44 -0600

    got end game screen in and functioning, also the knight and everything is working

--- WitchAndKnight/Assets/Nic/EnemyHealthNic.cs
+++ WitchAndKnight/Assets/Nic/EnemyHealthNic.cs
@@ -19,6 +19,14 @@ public class EnemyHealthNic : MonoBehaviour {
 	public GameObject EnemyHealthText;
 	private Text enemyHealthText;
 	private Vector3 respawnLocation;
+
+	// Two public variables that we can set in the editor (on a per-instance basis to change the properties of each platform we drop in the level).
+	public Vector3 direction;    // Controls the direction in X/Y/Z coordinates 
+	public int movementTime;    // Controls the duration (in seconds) that the platform will move for before returning
+	
+	// Private variables to track the timing & direction of the platform 
+	private float timer = 0;
+	private bool outgoing = true;
 	
 	void Start () {
 		
@@ -41,6 +49,38 @@ public class EnemyHealthNic : MonoBehaviour {
 			{
 			projHit = false;
 			}
+
+
+		// If the platform is outgoing aka. moving from its origin toward its destination
+		if (outgoing) {
+			
+			// Basic "Translate" function that will move the object in 3D space by the ammounds specified in the Vector3 (X,Y,Z)
+			// Mulitplied by delta Time to compensate for any fluctuations in frame rate 
+			this.gameObject.transform.Translate (direction * Time.deltaTime);
+			
+			// Increase the timer by delta time each frame, this effectively counts accurately in seconds
+			timer += Time.deltaTime;
+			
+			// If we've reached the end of our movement time we'll want to trip the "return" switch and set our outgoing boolean to false, also reset timer
+			if (timer > movementTime){
+				outgoing = false;
+				timer = 0;
+			}
+		}
+		
+		// If the platform is returning to the origin
+		else{
+			
+			// Translate the platform along the inverted direction (that's what the * -1 is doing)
+			this.gameObject.transform.Translate (direction * Time.deltaTime * -1);
+			
+			timer += Time.deltaTime;
+			
+			if (timer > movementTime){
+				outgoing = true;
+				timer = 0;
+			}
+		}
 	
 		}
 	
--- WitchAndKnight/Assets/Nic/NewGame.cs
+++ WitchAndKnight/Assets/Nic/NewGame.cs
@@ -5,7 +5,7 @@ public class NewGame : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		Application.LoadLevel ("NicsZone");
 	}
 	
 	// Update is called once per frame
