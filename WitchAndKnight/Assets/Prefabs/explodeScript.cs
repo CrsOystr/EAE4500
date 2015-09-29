35f51415a5402717355423095cfd6b10dc101dae
tatousolitaire <fdraservelt@gmail.com>
2015-09-29 17:49:05 -0600

    just scripts, mats, and prefabs
    for the particle system

---
+++ WitchAndKnight/Assets/Prefabs/explodeScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class explodeScript : MonoBehaviour {
+
+	public float timeToLive;
+	private float startTime;
+
+	// Use this for initialization
+	void Start () {
+
+			ParticleSystem exp = GetComponent<ParticleSystem>();
+			exp.Play();
+			startTime = Time.time;	
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time - startTime > timeToLive) {
+			Destroy(this.gameObject);
+				}
+	
+	}
+}
